90c14a28d7ff4ee81ed2ad73cf17cefd84117f44
Dhruv Sangamwar <dsangamwar@gmail.com>
2023-11-16 17:44:53 -0800

    Fixed the class hierarchy
    The library is functional for the end user but they have the wrap the main in a tokio::main macro
    to be able to make async calls and awat the response from the api.
    Next Steps: Need to do type coercion or conversion to make it simpler for end user to interface with data
    returning from the API.
    -Dhruv

--- testsdk/src/main.rs
+++ testsdk/src/main.rs
@@ -1,10 +1,27 @@
 // This is the main file for a project that uses the library.
 use resdb_rust_sdk::ResDB;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Specify the URL of the JSON API endpoint
-    let api_url = "https://crow.resilientdb.com/v1/transactions";
+    let database_url = "https://crow.resilientdb.com/v1/transactions";
     let res_db = ResDB::new(database_url);
-    res_db.get_all_transactions();
+    let transactions = res_db.get_all_transactions();
+    
+    // Call the asynchronous function and await the result
+    match res_db.get_all_transactions().await {
+        Ok(transactions) => {
+            if let Some(first_transaction) = transactions.first() {
+                // Access fields of the first transaction (replace with the desired field)
+                println!("{:?}", first_transaction.inputs);
+            } else {
+                println!("No transactions available");
+            }
+        }
+        Err(error) => {
+            // Handle the error
+            eprintln!("Error: {}", error);
+        }
+    }
 
 }
