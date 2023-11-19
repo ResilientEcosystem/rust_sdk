1134fad9791e6c24947fcda997717543b683b740
UshaSah <usah@ucdavis.edu>
2023-11-19 12:43:01 -0800

    can access local transactions.json

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,1 +1,67 @@
-// write rust code to save the Json response from the blocks endpoint into rust objects
+// // write rust code to save the Json response from the blocks endpoint into rust objects
+// use std::fs::File;
+// use std::io::Read;
+// use serde::Deserialize;
+// use anyhow::Error;
+
+// #[derive(Debug, Deserialize)]
+
+// pub struct Blocks{
+
+// }
+
+// pub struct Transaction {
+//     pub cmd: String,
+//     pub key: String,
+// }
+
+// pub struct Blocks {
+//     pub id: i32,
+//     pub number: String,
+//     pub transactions: Vec<Transaction>,
+//     pub size:i32,
+//     pub createdAt: String,
+// }
+
+// impl Blocks {
+//     pub fn new() -> Self {
+
+//         Blocks {
+//             id: i32:: new(),
+//             number: String::new(),
+//             transactions: Vec::new(),
+//             size: i32::new(),
+//             createdAt: String::new(),
+//         }
+    
+//     }
+// }
+ 
+// //  implementation of GEt v1/blocks
+// pub async fn get_all_blocks(api_url: &str) -> Result<Vec<Blocks>, anyhow::Error> {
+//     let response_text = reqwest::get(api_url).await?;
+
+//     let mut file = File::open("Users/mac/Desktop/ecs189f/resdb_rust_sdk/json_data/blocks.json").expect("Unable to open the file");
+//     // Read the file content into a string
+//     let mut content = String::new();
+//         file.read_to_string(&mut content).expect("Unable to read the file");
+
+//         let json_array: Vec<serde_json::Value> = serde_json::from_str(&content)?;
+//         let mut blocks = Vec::new();
+    
+//     for json_obj in json_array{
+//         let block: Blocks = serde_json::from_value(json_obj)?;
+//         blocks.push(block);
+//     }
+    
+//     // let response: Result<Transaction, serde_json::Error> = serde_json::from_str(&content);
+//     let response: Result<Vec<Blocks>, serde_json::Error> = Ok(blocks);
+
+//     // Handle the deserialization result
+//     match response {
+//         Ok(parsed_response) => Ok(parsed_response),
+//         Err(err) => Err(anyhow::Error::from(err)),
+//     }
+// }
+
+
--- testsdk/src/main.rs
+++ testsdk/src/main.rs
@@ -1,19 +1,46 @@
 // This is the main file for a project that uses the library.
-extern crate resdb_rust_sdk;
-use tokio::main;
+// extern crate resdb_rust_sdk;
+// use tokio::main;
 
-#[tokio::main]
-async fn main() {
-    // Specify the URL of the JSON API endpoint
-    let api_url = "https://crow.resilientdb.com/v1/transactions";
+// #[tokio::main]
+// async fn main() {
+//     // Specify the URL of the JSON API endpoint
+//     let api_url = "https://crow.resilientdb.com/v1/transactions";
 
-    match resdb_rust_sdk::get_data_from_api(api_url).await {
-        Ok(response) => {
-            println!("Response: {:?}", response);
-            // Handle the response data as needed
+//     match resdb_rust_sdk::get_data_from_api(api_url).await {
+//         Ok(response) => {
+//             println!("Response: {:?}", response);
+//             // Handle the response data as needed
+//         }
+//         Err(err) => {
+//             eprintln!("Error: {:?}", err);
+//             // Handle the error
+//         }
+//     }
+// }
+use std::fs;
+use serde_json::Value; // Assuming you're using serde to handle JSON
+
+fn main() {
+    // Read the contents of the local JSON file
+    let file_contents = match fs::read_to_string("./transactions.json") {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("Error reading file: {:?}", err);
+            return;
+        }
+    };
+
+    // Parse the JSON data from the file
+    let parsed_data: Result<Value, _> = serde_json::from_str(&file_contents);
+
+    match parsed_data {
+        Ok(json_data) => {
+            println!("Parsed data: {:?}", json_data);
+            // Handle the parsed data as needed
         }
         Err(err) => {
-            eprintln!("Error: {:?}", err);
+            eprintln!("Error parsing JSON: {:?}", err);
             // Handle the error
         }
     }
